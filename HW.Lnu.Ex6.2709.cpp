8f22daea171c7d56dbdba63dd594af4fac202f13
S1NEDIO <43414527+S1NEDIO@users.noreply.github.com>
2018-09-26 22:22:04 +0300

    Create HW.Lnu.Ex6.2709.cpp

---
+++ HW.Lnu.Ex6.2709.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int a;
+	cout << "Input a" << endl;
+	cin >> a;
+	bool res;
+	res = (a >= 1000);
+	cout << res << endl;
+	system("pause");
+}
