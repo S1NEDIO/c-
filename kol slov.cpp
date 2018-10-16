c16c558904d16dbada6d45429968655c8c37bab6
S1NEDIO <43414527+S1NEDIO@users.noreply.github.com>
2018-10-16 20:10:34 +0300

    Create kol slov.cpp

---
+++ kol slov.cpp
@@ -0,0 +1,25 @@
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char m[100];
+	int a, i, k = 1;
+	cout << "Vvedite stroku:" << endl;
+	gets_s(m);
+	a = strlen(m);
+	for (i = 0; i < a; i++)
+	{
+		if (m[i] == ' ')
+			k++;
+		else if (m[i] == ',')
+			k++;
+		else if (m[i] == '.')
+			k++;
+		else if (m[i] == '\n')
+			k++;
+	}
+	cout << "Koli4estvo slov v stroke=" << k << endl;
+
+	_getch();
+	return 0;
+}
